1c2e9de4bc24c9664cf86138c75e4b90a362d7b5
Pedro Dantas <pedro2008hds@gmail.com>
2026-05-10 16:55:09 -0300

    Add files via upload

--- test_exercicio1.py
+++ test_exercicio1.py
@@ -1,14 +1,16 @@
 from exercicio1 import acao_semaforo
 
-def test_cor_vermelho():
-    assert acao_semaforo('vermelho') == 'Pare'
+def test_vermelho():
+    assert acao_semaforo("vermelho") == "Pare"
 
-def test_cor_amarelo():
-    assert acao_semaforo('amarelo') == 'Atenção'
 
-def test_cor_verde():
-    assert acao_semaforo('verde') == 'Siga'
+def test_amarelo():
+    assert acao_semaforo("amarelo") == "Atenção"
 
-def test_cor_errada():
-    assert acao_semaforo('rosa') == 'Cor inválida'
 
+def test_verde():
+    assert acao_semaforo("verde") == "Siga"
+
+
+def test_cor_invalida():
+    assert acao_semaforo("azul") == "Cor inválida"
